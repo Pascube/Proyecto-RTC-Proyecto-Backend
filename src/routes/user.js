3faff4840c1483af2c99cf95b272d0a3810ca504
David Pascual <david.pascual@octopusenergy.services>
2026-06-22 09:08:04 +0200

    fix: CRUD completo, validacion deleteUser, isModified password, seed con connectDB

---
+++ src/routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { register, login, getUsers, deleteUser, addFavorite, updateRole, updateUser } = require('../controllers/user');
+const { isAuth, isAdmin } = require('../middlewares/auth');
+const { upload } = require('../config/cloudinary');
+
+const userRoutes = express.Router();
+
+userRoutes.post('/register', upload.single('image'), register);
+userRoutes.post('/login', login);
+userRoutes.get('/', isAuth, isAdmin, getUsers);
+userRoutes.delete('/:id', isAuth, deleteUser);
+userRoutes.post('/favorites', isAuth, addFavorite);
+userRoutes.put('/role/:id', isAuth, isAdmin, updateRole);
+userRoutes.put('/:id', isAuth, upload.single('image'), updateUser);
+
+module.exports = userRoutes;
