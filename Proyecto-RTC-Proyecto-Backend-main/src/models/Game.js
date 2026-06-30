ef5395f9f397530ab62cfc894375ab498bd35b40
David Pascual <david.pascual@octopusenergy.services>
2026-06-30 17:58:02 +0200

    chore: remove duplicated nested project folder

--- Proyecto-RTC-Proyecto-Backend-main/src/models/Game.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const gameSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  genre: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Game', gameSchema);
